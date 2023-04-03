0b176e6f27ef69977d30a66030a1c038569ba6d0
c.michel <2120533@stud.hs-mannheim.de>
2023-04-03 17:47:18 +0200

    working version

--- TRNG-Pendel/ReadThePendulum.py
+++ TRNG-Pendel/ReadThePendulum.py
@@ -9,38 +9,37 @@ if os.path.exists(filename):
         os.remove(filename)
         print(f"File '{filename}' has been deleted.")
 
+allList = []
 
-
-# # open the default camera (usually the built-in webcam)
-# cap = cv.VideoCapture(1, cv.CAP_DSHOW)
-
-# # check if camera opened successfully
-# if not cap.isOpened():
-#     print("Error opening video capture.")
-
-# # read a single frame from the camera
-
-
-# # name = 'a'
-# count = 0
-# # if frame is read correctly, show it
-# while(True):
-#       ret, frame = cap.read()
-#       # cv2.imshow("Webcam", frame)
-#       # cv2.waitKey(0)
-#       cv.imwrite('C:\\Python311\\Tests\\Pictures\\' + str(count) + '.jpg', frame) #save image
-#       # name = name + 'a'
-#       count = count+1
-#       if(count > 100):
-#          break
-   
-# # else:
-#    # print("Error reading frame from video capture.")
-
-# # release the camera and close the window
-# cap.release()
-# cv.destroyAllWindows()
-
+### open the default camera (usually the built-in webcam)
+##cap = cv.VideoCapture(1, cv.CAP_DSHOW)
+### check if camera opened successfully
+##if not cap.isOpened():
+##    print("Error opening video capture.")
+##
+### read a single frame from the camera
+##
+##
+### name = 'a'
+##count = 0
+### if frame is read correctly, show it
+##while(True):
+##      ret, frame = cap.read()
+##      # cv2.imshow("Webcam", frame)
+##      # cv2.waitKey(0)
+##      cv.imwrite('C:\\Python311\\Tests\\Pictures\\' + str(count) + '.jpg', frame) #save image
+##      # name = name + 'a'
+##      count = count+1
+##      if(count > 100):
+##         break
+##   
+### else:
+##   # print("Error reading frame from video capture.")
+##
+### release the camera and close the window
+##cap.release()
+##cv.destroyAllWindows()
+##
 
 
 #Read the Folder
@@ -56,11 +55,11 @@ def sha512Hash(data):
     
 def loadImage(path):
     im = Image.open(path)
-    print("Das Bild wurde geladen" + path)
+    # print("Das Bild wurde geladen" + path)
     return im
     
 def scanResolution(image):
-    print("Folgende Auflösung wurde erkannt: Breite="+str(image.size[0])+" Höhe="+str(image.size[1]))
+    # print("Folgende Auflösung wurde erkannt: Breite="+str(image.size[0])+" Höhe="+str(image.size[1]))
     return image.size
 
 def getWidth(image):
@@ -74,26 +73,19 @@ def scanBlackDots(im, width, height):
     dotsAvg = []
     rgb_im = im.convert("RGB")
 
-    print("Beginning to search for Black Dots")
+    # print("Beginning to search for Black Dots")
 
-    prevX = 0
-    prevY = 0
-    
     for x in range(width):
-    #print(str(x)+" von "+str(width))
-    #print("%3i Prozent" % ((x/width)*100))
+  
         for y in range(height):
             r, g, b = rgb_im.getpixel((x, y))
-            #print("Pixelkoords: : %3s %3s Rot: %3s Grün: %3s Blau: %3s" % (x,y,r,g,b))
+            
             if((r >=0 and g >= 0 and b >= 120)and(r <= 55 and g <= 200 and b <= 255) ):
-                print("Schwarzer Pixel an X: "+str(x)+" und Y: "+str(y))
+                # print("Schwarzer Pixel an X: "+str(x)+" und Y: "+str(y))
                 counter, summeX, summeY = counter+1, summeX+x, summeY+y
                 avg = summeX / counter
-                prevX = summeX/counter
-                prevY = summeY/counter
-                if x < avg - 150 or x > avg + 150:
-                   if prevX != (summeX/counter) and prevY != (summeY/counter):
-                       print(prevX)
+               
+                if x < avg - 100 or x > avg + 100:
                        dotsAvg.append((summeX/counter))
                        dotsAvg.append((summeY/counter))
                        summeX = summeY = counter = 0
@@ -101,31 +93,25 @@ def scanBlackDots(im, width, height):
     if not counter == 0:         
         dotsAvg.append((summeX/counter))
         dotsAvg.append((summeY/counter))        
-        print(str(int(len(dotsAvg)/2))+" Schwarze Punkte wurden gefunden")
+        # print(str(int(len(dotsAvg)/2))+" Schwarze Punkte wurden gefunden")
     
     return dotsAvg
     
 def printResults(dotsAvg):
     counter, punkt = 0, 1
+    counter1 = 0
+          
     for z in dotsAvg:
         if(counter % 2 == 0):
-            print("Punkt "+str(punkt)+" X-Kord: "+str(int(z)))
-        else:
-            print("Punkt "+str(punkt)+" Y-Kord: "+str(int(z)))
+            # print("Punkt "+str(punkt)+" X-Kord: "+str(int(z)))
+            allList.append(int(z))                      
+        else:                
+            # print("Punkt "+str(punkt)+" Y-Kord: "+str(int(z)))
+            allList.append(int(z))
             punkt += 1
         counter += 1
     string = ""
-    for z in dotsAvg:
-        string += sha512Hash(z)
-    print(string)
-
-    
-    filename='C:\\Python311\\Tests\\Output\\rawNumbers.txt'
-
-              
-    outputFile= open('C:\\Python311\\Tests\\Output\\rawNumbers.txt',"a")
-    outputFile.write(string)
-    outputFile.close()
+   
 
 
     
@@ -166,7 +152,7 @@ for filename in os.listdir(directory):
 ## checking if it is a file
 
     if os.path.isfile(f):
-        print(f)
+
     
         im = loadImage('C:\\Python311\\Tests\\Pictures\\'+str(x)+".jpg")
         x=x+1
@@ -174,9 +160,48 @@ for filename in os.listdir(directory):
         result = scanBlackDots(im, getWidth(im), getHeight(im))
         printResults(result)
 
+def deleteDoubles(allList):
 
-print(comparePictures("test9.png","test10.png"))
+
+# Initialisiere eine leere Liste, um die Ergebnisse zu speichern
+    pairs = []
+
+# Iteriere über die Zahlenliste in Schritten von 2, da jedes Paar aus zwei Zahlen besteht
+    for i in range(0, len(allList), 2):
+        pair = [allList[i], allList[i+1]]
+    # Überprüfe, ob das aktuelle Paar bereits in der Liste `pairs` vorhanden ist
+        if pair not in pairs:
+            pairs.append(pair)
+
+# Gib die Liste der einzigartigen Zweierpaare aus
+    print(pairs)
+
+    return pairs
 
 
-###############
+coordinates = deleteDoubles(allList)
 
+def writeOutput(coordinates):
+    output = []
+    for c in coordinates:
+        output.append(sha512Hash(c))     
+        string = str(output)
+        string=string.replace(" ",'')
+        string=string.replace(",",'')
+        string=string.replace("'",'')
+        string=string.replace("]",'')
+        string=string.replace("[",'')
+
+
+
+
+
+
+        print(string)
+    with open("C:\Git\TrueRandomNumberGenerator\Output\\rawNumbers.txt", "w") as f:
+        f.write(string)
+        os.close    
+      
+    return
+
+writeOutput(coordinates)
