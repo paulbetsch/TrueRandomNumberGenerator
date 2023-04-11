8f2d75fdf9ffe7b597cd34062d9c20844f5e07d5
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-04-11 08:31:15 +0200

    Update ReadThePendulum.py
    Update Class

--- TRNG-Pendel/ReadThePendulum.py
+++ TRNG-Pendel/ReadThePendulum.py
@@ -2,241 +2,193 @@ from PIL import Image
 import hashlib
 import os.path
 import cv2 as cv
-
-current_dir = os.getcwd()
-pictures_folder_path = os.path.join(current_dir, "Pictures")
-
-
-# Löscht inhalte der rawnumbers.txt, für neuen Output
-
-def create_output():
-    filename = os.getcwd() + '\\rawnumbers.txt'
-
+import math
+import struct
+import hashlib
+import numpy as np
+import csv
+ 
+# Läd das Bild des übergebenen Pfades und gibt es zurück
+def DeleteFileContents(filename):
+    filename = os.getcwd() + '\\' + filename
     if os.path.exists(filename):
-        os.remove(filename)
-        print(f"File '{filename}' has been deleted.")
-
-    with open(filename, 'w') as file:
-        print(f"File '{filename}' has been created.")
-
-# Macht bilder und schreibt sie in den Pictures Ordner, wird nur bei Live auswertung benötigt
-
-def take_pictures():
-    cap = cv.VideoCapture(1, cv.CAP_DSHOW)
-
-    # check if camera opened successfully
-    if not cap.isOpened():
-        print("Error opening video capture.")
-
-    # read a single frame from the camera
-
-    # name = 'a'
-    count = 0
-    # if frame is read correctly, show it
-    while True:
-        ret, frame = cap.read()
-        # cv2.imshow("Webcam", frame)
-        # cv2.waitKey(0)
-        cv.imwrite(os.getcwd() + '\\Pictures\\' + str(count) + '.png', frame)  # save image
-        # name = name + 'a'
-        count = count + 1
-        if count > 100:
-            break
-
-
-    # release the camera and close the window
-    cap.release()
-    cv.destroyAllWindows()
-
-
-# Löscht alte Bilder aus dem Pictures Ordner, für Live analyse benötigt
-
-def delete_contents():
-    for filename in os.listdir(pictures_folder_path):
-        file_path = os.path.join(pictures_folder_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                os.rmdir(file_path)
-        except Exception as e:
-            print(f"Failed to delete {file_path}. Reason: {e}")
-        print("Picture deleted")
-
-
-# Erstellt Pictures ordner falls nicht vorhanden
-
-def folder():
-    if os.path.exists(pictures_folder_path) and os.path.isdir(pictures_folder_path):
-        print("Folder found: " + pictures_folder_path)
+        with open(filename, 'w') as file:
+            file.write('')
+            print(filename + " cleared")
     else:
-        os.mkdir('Pictures')
-        os.listdir('Pictures')
+        print(filename + " not found")
+ 
+def LoadImage(path):
+    try:
+        im = Image.open(path)
+    except FileNotFoundError  as e:
+        print(" ")
+        print(str(path) + " not found .")
+    else:
+        print(" ")
+        print(str(path) + " loaded.")
+        return im
+    return 0
+ 
+def GetWidth(image):
+    return image.size[0]
+ 
+def GetHeight(image):
+    return image.size[1]
+ 
+#Returns Array with RGB values for Pendelum points
+ 
+def GetRgbValues():
+    #rgbBlue = [0, 55, 90, 66, 200, 110]
+    #rgbRed = [80, 0, 0, 110, 10, 15]
+    #rgbAll = [rgbBlue, rgbRed]
+ 
+    rgbGreen = [40, 110, 95, 65, 125, 100]
+    rgbYellow = [190, 167, 65, 220, 185, 100]
+    rgbBlue = [45, 110, 135, 70, 130 ,160]
+    rgbAll = [rgbBlue, rgbYellow, rgbGreen]
+ 
+    return rgbAll
+ 
+# Sucht nach den übergebenen RGB werten in dem übergebenen Bild und gibt ein Array mit den Koordinaten
+# der jeweilig gefunden Punkte für die RGB werte zurück
+ 
+def ScanDots(image, width, height, rgb):
+    coords = []
+    # für jede RGB reichtweite ein durchlauf
+    for i in range(len(rgb)):
+        counter = summeX = summeY = 0
+        dotsAvg = []
+        np_im = np.array(image)
+        # RGB reichweiten deklarieren
+        mask = np.logical_and.reduce((np_im[:,:,0] >= rgb[i][0], np_im[:,:,1] >= rgb[i][1], np_im[:,:,2] >= rgb[i][2],
+                                      np_im[:,:,0] <= rgb[i][3], np_im[:,:,1] <= rgb[i][4], np_im[:,:,2] <= rgb[i][5]))
+        pixels = np.transpose(np.where(mask))
+        for x, y in pixels:
+            counter, summeX, summeY = counter + 1, summeX + x, summeY + y
+            avg = summeX / counter
+ 
+            if x < avg - 70 or x > avg + 70:
+                dotsAvg.append((summeX / counter))
+                dotsAvg.append((summeY / counter))
+                summeX = summeY = counter = 0
+ 
+        if not counter == 0:
+            dotsAvg.append((summeX / counter))
+            dotsAvg.append((summeY / counter))
+            print(str(int(len(dotsAvg) / 2)) + " Punkt wurde für "  + str(rgb[i][0]) + " =< R >= " + str(rgb[i][1]) + " " + 
+                str(rgb[i][2]) + " =< G >= " + str(rgb[i][3]) + " " +str(rgb[i][4]) + " =< B >= " + str(rgb[i][5]) + " gefunden")
+            if (int(len(dotsAvg) / 2)) == 1:
+                print("Valid coords - appended")
+                coords.append(dotsAvg)
+ 
+        # Gefundener Punkt (dotsAvg) x,y anhängen --> dann nächste Farbe      
+    return coords
+ 
+# Vergleicht zwei Bilder auf ausreichende Pendel bewegung
+ 
+def Analyse(file, pictureName, idx, endIdx):
+    rgb = GetRgbValues()
+    all_results = []
+    dir = os.getcwd() + '\\TRNG-Pendel\\' + file + '\\'
+    while(idx < endIdx):
+        image = LoadImage(dir + pictureName + str(idx) + ".jpg")
+        if image != 0:
+            result = ScanDots(image, GetWidth(image), GetHeight(image), rgb)
+            all_results.append(result)
+            WriteToCsv(result)
+        idx += 1
+    return all_results
+ 
+# Schreibt Koordinaten für 1 Bild in jeweilige csv Dateien für die jeweilige Farbe
+ 
+def WriteToCsv(coords):
+     for i, lst in enumerate(coords):
+                filename = f"Koordinaten_{i+1}.csv"
+                with open(filename, 'a', newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile)
+                    for j in range(0, len(lst), 2):
+                        csvwriter.writerow([lst[j], lst[j+1]])
+                    print(f"written to {filename} successfully.")
+ 
+def WriteToFile(input, filename):
+ 
+    filename = os.getcwd() + '\\' + filename
+ 
+    with open(filename, 'a') as file:
+        print(f"File '{filename}' write to file.")
+    outputFile = open(filename, "a")
+    outputFile.write(str(input))
+    outputFile.close()
+ 
+# Methode um Koordinaten sauber auszugeben
+def FloatToBinary(number):
+    packed = struct.pack('f', number)
+    number = ''.join(format(b, '08b')for b in packed)
+    return number
+ 
+def LsbBits(results):
+    DeleteFileContents("binarynumbers")
+    bits = []
+    for result in results:
+        for coords in result:
+            for coord in coords:
+                binary_str = format(struct.unpack('!Q', struct.pack('!d', coord))[0], '064b')
+                lsb = int(binary_str[-1])
+                print(f"Original float: {coord}")
+                print(f"Binary string: {binary_str}")
+                print(f"LSB: {lsb}")
+                WriteToFile(lsb, "binarynumbers")
+                bits.append(lsb)
+    return bits
+ 
+def xor_binary_files(file1, file2, output_file):
+    with open(file1, 'rb') as f1, open(file2, 'rb') as f2, open(output_file, 'wb') as out:
+        byte1 = f1.read(1)
+        byte2 = f2.read(1)
+        while byte1 and byte2:
+            out.write(bytes([byte1[0] ^ byte2[0]]))
+            byte1 = f1.read(1)
+            byte2 = f2.read(1)
+ 
+def HashBits(results):
+    DeleteFileContents("binarynumbers")
+    gangweite = 0
+    for result in results:
+        for coords in result:
+            for coord in coords:
+                prevcoord = coord
+                if gangweite == 10:
+                    num = bytes(str(coord), 'utf-8')
+                    seed = struct.pack('f', prevcoord)
+ 
+                    hash_object = hashlib.blake2b(digest_size=8, salt=seed)
+                    hash_object.update(str(num).encode())
+                    hash_value = hash_object.digest()
+ 
+                    hash_int = int.from_bytes(hash_value, byteorder='big')
+                    hash_bits = bin(hash_int)[2:]
+                    WriteToFile(hash_bits, "binarynumbers")
+                else: 
+                    gangweite += 1
+                
+def WriteToCsv(results):
+     for coords in results:
+        for i, lst in enumerate(coords):
+                    filename = f"Koordinaten_{i+1}.csv"
+                    with open(filename, 'a', newline='') as csvfile:
+                        csvwriter = csv.writer(csvfile)
+                        for j in range(0, len(lst), 2):
+                            csvwriter.writerow([lst[j], lst[j+1]])
+                        print(f"written to {filename} successfully.")
 
-
-def sha512Hash(data):
+def Sha512Hash(data):
     hashValue = hashlib.sha512(str(data).encode('utf-8')).hexdigest()
     res = bin(int('1' + hashValue, 16))[3:]
+    WriteToFile(res, "binarynumbers")
     return res
-
-
-def loadImage(path):
-    im = Image.open(path)
-    print(str(path) + " picture has been loaded.")
-    return im
-
-
-def scanResolution(image):
-    print("Dected resolution: Width=" + str(image.size[0]) + " Height=" + str(image.size[1]))
-    return image.size
-
-
-def getWidth(image):
-    return image.size[0]
-
-
-def getHeight(image):
-    return image.size[1]
-
-
-def scanBlackDots(im, width, height):
-    counter = summeX = summeY = 0
-    dotsAvg = []
-    rgb_im = im.convert("RGB")
-
-    # print("Beginning to search for Black Dots")
-
-    for x in range(width):
-        for y in range(height):
-            r, g, b = rgb_im.getpixel((x, y))
-            if (r >= 0 and g >= 0 and b >= 120) and (r <= 55 and g <= 200 and b <= 255):
-                #print("Blau Pixel an X: " + str(x) + " und Y: " + str(y))
-                counter, summeX, summeY = counter + 1, summeX + x, summeY + y
-                avg = summeX / counter
-
-                if x < avg - 30 or x > avg + 30:
-                    dotsAvg.append((summeX / counter))
-                    dotsAvg.append((summeY / counter))
-                    summeX = summeY = counter = 0
-
-    if not counter == 0:
-        dotsAvg.append((summeX / counter))
-        dotsAvg.append((summeY / counter))
-        print(str(int(len(dotsAvg) / 2)) + " Schwarze Punkte wurden gefunden")
-
-    return dotsAvg
-
-
-def anlayzeMovement(pic1, pic2):
-    pic1 = loadImage(pic1)
-    pic2 = loadImage(pic2)
-
-    widthPic1 = getWidth(pic1)
-    heightPic1 = getHeight(pic1)
-    widthPic2 = getWidth(pic2)
-    heightPic2 = getHeight(pic2)
-
-    ##In Case of a camera error, in the normal state of the camer
-    ##it will always stay the same resolution!
-    if (widthPic1 != widthPic2 and heightPic1 != heightPic2):
-        printErrorMessage("Stopped Process", "Camera Failure", "The resolution of the processed picture are different!",
-                          ("Picture 1: " + str(widthPic1) + "x" + str(heightPic1)),
-                          ("Picture 2: " + str(widthPic2) + "x" + str(heightPic2)))
-        return False
-
-    dotsPic1 = scanBlackDots(pic1, widthPic1, heightPic1)
-    dotsPic2 = scanBlackDots(pic2, widthPic2, heightPic2)
-
-    ##If the programm does not find the same Amount of black Dots
-    ##there has to be movement in the pictures.
-    if (len(dotsPic1) != len(dotsPic2)):
-        return True
-
-    ##Analyze the Coordinates of the black dots if the dots cords
-    ##differentiate over more than 15 Pixels the programm we accept
-    ##it as Movement.
-    for i in range(len(dotsPic1)):
-        if not ((dotsPic2[i] + 15) >= dotsPic1[i] >= (dotsPic2[i] - 15)):
-            print("Die Bilder unterscheiden sich gut genug. Breite: " + str(dotsPic1[i]) + " Höhe: " + str(dotsPic2[i]))
-            return True
-
-    return False
-
-
-def printErrorMessage(*string):
-    header = "#"
-    header = header * (len(max(string, key=len)) + 4)
-    print(header)
-    for s in string:
-        print("# " + str(s))
-    print(header)
-
-
-
-
-
-# Bilder richtig auf name anpassen
-def analyse():
-    x = 100
-    help = []
-    directory = os.getcwd() + '\\Pictures\\'
-    for filename in os.listdir(directory):
-        if x > 650:
-            break
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            image = loadImage(directory + "Test" + str(x) + ".png")
-            x = x + 1
-            result = scanBlackDots(image, getWidth(image), getHeight(image))
-            bool = True
-            for i in help:
-                if i == result:
-                    bool = False
-                    break
-            if bool == True:
-                help.append(result)
-    for i in help:
-        print_results(i)
-
-
-    clearOutput(help)
-
-
-def clearOutput(help):
-    for i in range(len(help)):
-        print(help[i])
-
-
-
-
-
-
-def print_results(dotsAvg):
-    counter, punkt = 0, 1
-    for z in dotsAvg:
-        if (counter % 2 == 0):
-            print("Punkt " + str(punkt) + " X-Kord: " + str(int(z)))
-        else:
-            print("Punkt " + str(punkt) + " Y-Kord: " + str(int(z)))
-            punkt += 1
-        counter += 1
-    string = ""
-    for z in dotsAvg:
-        string += sha512Hash(z)
-    print(string)
-
-    filename = os.getcwd() + '\\rawNumbers.txt'
-
-    outputFile = open(os.getcwd() + '\\rawNumbers.txt', "a")
-    outputFile.write(string)
-    outputFile.close()
-
-
-def run():
-    folder()
-    create_output()
-    # delete_contents()
-    analyse()
-
-
-run()
+ 
+def Run():
+    results = Analyse("TriPendelGelb", "", 1, 150)
+    HashBits(results)
+Run()
