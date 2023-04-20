df9101d108a32984c41e805b31ad28e3bbd85d59
Paul Betsch <2120546@stud.hs-mannheim.de>
2023-04-20 11:19:41 +0200

    Codestyle applied

--- TRNG-Pendel/Lightbarrier/Multiprocessing.py
+++ TRNG-Pendel/Lightbarrier/Multiprocessing.py
@@ -3,34 +3,41 @@ from multiprocessing import Queue
 import multiprocessing
 import time
 
-def print1000(name, list):
-    starttime = time.time()
-    print(str(name)+" Starting "+ str(starttime))
+# This function is just to figure out the beviour of different processes
+# It is important to know when the process is being started and when it 
+# finishes his tasks.
+def UselessPrints(name, list):
+    startTime = time.time()
+    print(str(name)+" Starting "+ str(startTime))
     for i in range(1000000):
         list.append(i)
-        continue
-    endtime = time.time()
-    print(str(name)+" Ended "+ str(endtime))
-    print(str(name)+" Time:"+str((endtime-starttime)))
+    endTime = time.time()
+    print(str(name)+" Ended "+ str(endTime))
+    print(str(name)+" Total Time:"+str((endTime-startTime)))
     
 sharedList = []
 
 if __name__ == "__main__":
+    # Opens a new Manger to allow interprocess communication
     with Manager() as manager:
+        # Two List to exchange data from the worker threads to the main threads
         sharedList = manager.list()
         sharedList2 = manager.list()
 
         procs = []
-        first = Process(target=print1000, args=("Process1", sharedList))
+        first = Process(target=UselessPrints, args=("Process1", sharedList))
         procs.append(first)
-        second = Process(target=print1000, args=("Process2", sharedList2))
+        second = Process(target=UselessPrints, args=("Process2", sharedList2))
         procs.append(second)
+
+        # Using start() to simutainiusly run the code instead of run()
         first.start()
         second.start()
 
         print("sleeping")
         time.sleep(4)
 
+        # Killing the threads after 4 Seconds
         print("killing")
         first.terminate()
         second.terminate()
@@ -39,38 +46,7 @@ if __name__ == "__main__":
         # for p in procs:
         #     p.join()
 
-        print("Main")
+        print("Main Thread")
         print(sharedList)
         print("neue Liste")
         print(sharedList2)
-
-
-
-#print(multiprocessing.cpu_count())
-
-# def print1000(name, stopEvent):
-#     starttime = time.time()
-#     print(str(name)+" Starting "+ str(starttime))
-#     for i in range(10000):
-#         #if(stopEvent.is_set()):
-#             #break
-#         continue
-#     endtime = time.time()
-#     print(str(name)+" Ended "+ str(endtime))
-#     print(str(name)+" Time:"+str((endtime-starttime)))
-    
-
-# if __name__ == "__main__":
-#     stopEvent = multiprocessing.Event()
-
-#     first = Process(target=print1000, args=("Process1", stopEvent))
-    
-#     second = Process(target=print1000, args=("Process2", stopEvent))
-#     first.start()
-#     second.start()
-
-#     first.join()
-#     second.join()
-
-#     print("Main")
-      
