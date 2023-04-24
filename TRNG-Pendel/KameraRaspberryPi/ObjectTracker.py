8df927d2ffb7e2ca813ccc832f4d4fc0be2c7ad3
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-04-24 14:06:23 +0200

    Update ObjectTracker.py
    - optimised and tested Code.
    - added comments

--- TRNG-Pendel/KameraRaspberryPi/ObjectTracker.py
+++ TRNG-Pendel/KameraRaspberryPi/ObjectTracker.py
@@ -3,90 +3,107 @@ import math
 import struct
 import time
 
-# Set up video capture from default camera
+# um Programm zu stoppen "q" in geöffnetem Fenster drücken 
+# Video Capture anpassen - 0 = Standard Kamera , 1 = Externe Kamera ...
 cap = cv2.VideoCapture(0)
+#RGB reichweite für Punkte
+LOWER_BLACK = (0, 0, 0)
+UPPER_BLACK = (255, 255, 80)
+# Mittelpunkt für Polar Koordinaten System
+X_MIDDLE = 290
+Y_MIDDLE = 215
+# Minimum Fläche für Punkt
+MIN_AREA = 100
 
-# Define lower and upper bounds of black color in HSV color space
-lower_black = (0, 0, 0)
-upper_black = (255,255,80)
-x_middle = 1169
-y_middle = 514
-# Set minimum area for contour detection
-min_contour_area = 75
+# Daten
+XCOORD_LIST = []
+YCOORD_LIST = []
+DISTANZ_LIST = []
+WINKEL_LIST = []
+TIMESTAMPS = []
 
-distanz = []
-winkel = []
-timestamp = []
-def coords():
-	n = 0
-	for x in distanz:
-		with open('distanz.csv', 'a') as f:
-				f.write(str(timestamp[n]) + ", " + str(x) + "\n")
-		n += 1
-	
-	n = 0
-	for i in winkel:
-		with open('winkel.csv', 'a') as f:
-				f.write(str(timestamp[n]) + ", " + str(i) + "\n")
-		n += 1 
+# Schreibt alle Daten in CSV Datei mit: timestamp, x, y, abstand, winkel
+def Coords():
+    # Überschreibt alte CSV Datei und schreibt Kopfzeile
+    with open('output.csv', 'w') as f:
+        f.write("timestamp, x, y, abstand, winkel" + "\n")
 
-def bin():
-	for x in winkel:
-		binary_str = ''.join(format(c, '08b') for c in struct.pack('!f', x))
-		if len(binary_str) > 8:
-			lsb = binary_str[-1]
-			print(str(lsb))
+    # Schreibt Daten in CSV
+    n = 0
+    for x in TIMESTAMPS:
+        with open('output.csv', 'a') as f:
+            #print(str(x) + ", " + str(xcoord_list[n]) + ", " + str(ycoord_list[n]) + ", " + str(distanz_list[n]) + ", " + str(winkel_list[n]) + "\n")
+            f.write(str(x) + ", " + str(XCOORD_LIST[n]) + ", " + str(YCOORD_LIST[n]) + ", " + str(DISTANZ_LIST[n]) + ", " + str(WINKEL_LIST[n]) + "\n")
+        n += 1
 
-			with open('bin.txt', 'a') as f:
-				f.write(lsb)
 
-while True:
-    # Read frame from camera
-    ret, frame = cap.read()
-    
-    # Convert frame to HSV color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    # Create a mask for black pixels
-    mask = cv2.inRange(hsv, lower_black, upper_black)
-    
-    # Find contours in the mask
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # Draw a circle around each big black object and track its position
-    for contour in contours:
-        # Compute the area of the contour
-        area = cv2.contourArea(contour)
+# Durchläuft liste mit Floats, schreibt lsbs in bin.txt
+def LsbFloat(liste):
+    # Löscht Inhalt bin.txt
+    #with open('bin.txt', 'w') as f:
+    #    f.write("")
+
+    # Schreibt lsbs
+    for i in liste:
+        binary_str = ''.join(format(c, '08b') for c in struct.pack('!f', i))
+        if len(binary_str) > 8:
+            lsb = binary_str[-1]
+            with open('bin.txt', 'a') as f:
+                f.write(lsb)
+
+def Capture():
+
+    while True:
+        ret, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, LOWER_BLACK, UPPER_BLACK)
+
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
-        # Only consider contours that meet the minimum area threshold
-        if area >= min_contour_area:
-            # Compute the center and radius of the contour
-            (x, y), radius = cv2.minEnclosingCircle(contour)
-            timestamp.append(time.time())
-            center = (float(x), float(y))
-            radius = int(radius)
-              # Draw a circle around the contour
-            cv2.circle(frame, (int(x), int(y)), radius, (0, 255, 0), 2)
-            
-            # Print the position of the center
-            print("Black object position: ({}, {})".format(center[0], center[1]))
-            dx = x_middle - float(x)
-            dy = y_middle - float(y)
-            abstand = math.sqrt(dx**2 + dy**2)
-            distanz.append(abstand)
-            x = math.acos(dx/abstand)
-            winkel.append(x)
-           
-     
-    
-    # Display the frame
-    cv2.imshow("Frame", frame)
-    
-    # Exit if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area >= MIN_AREA:
+                # Compute the center and radius of the contour
+                (x, y), radius = cv2.minEnclosingCircle(contour)
+                TIMESTAMPS.append(time.time())
+                center = (float(x), float(y))
+                radius = int(radius)
+                
+                # Kreis um Punkt
+                cv2.circle(frame, (int(x), int(y)), radius, (0, 255, 0), 2)
+                
+                # Linie zu Punkt
+                cv2.line(frame, (int(X_MIDDLE), int(Y_MIDDLE)), (int(x), int(y)), (0, 0, 255), 2)
+                
+                dx = X_MIDDLE - float(x)
+                dy = Y_MIDDLE - float(y)
+                distanz = math.sqrt(dx ** 2 + dy ** 2)
+        
+                winkel = math.degrees(math.atan2(dy, dx))  # Winkel berechnung in Gradmaß
+                winkel = (winkel + 360) % 360  # Gradmaß zu 360 Grad System (anstatt 0-180 und 0 - (-180))
+                angleStr = "Winkel: " + str(round(x, 2)) + " Grad"
+                cv2.putText(frame, angleStr, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                
+                #Ausgabe 
+                print("Black object position: x " + str(float(x)) + " y " + str(float(y)))
+                print("Black Object winkel: " + str(winkel) + " abstand " + str(distanz)) 
+                XCOORD_LIST.append(float(x))
+                YCOORD_LIST.append(float(y))
+                DISTANZ_LIST.append(distanz)
+                WINKEL_LIST.append(winkel)
+                # Abfrage geschwindigkeit anpassen
+                time.sleep(0.008)
+                
+        cv2.imshow("Frame", frame)
+        # when q gedrückt beenden
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-bin()
-coords()
-cap.release()
-cv2.destroyAllWindows()
+def __main__():
+    Capture()
+    LsbFloat(WINKEL_LIST)
+    Coords()
+
+__main__()
