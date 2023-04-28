bbe81cb7514d4af5c514e0cceefab3b4e2509893
paulbetsch <128893709+paulbetsch@users.noreply.github.com>
2023-04-28 09:17:11 +0200

    Lsb Sampler to txt File

--- TRNG-Pendel/Lightbarrier/lsbsampler.py
+++ TRNG-Pendel/Lightbarrier/lsbsampler.py
@@ -3,6 +3,7 @@ def decListToBinaryList(listValues):
     s = ""
     for v in listValues:
         s += format(v, 'b')[-1]
-    print(s)
+    with open('lichtschranke.txt', 'w') as f:
+        f.write(s)
     
     
