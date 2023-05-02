385efad266e9e02619c657df2398f145a65e6357
Marina <2023503@stud.hs-mannheim.de>
2023-05-02 10:09:22 +0200

    Code Style und Kommentare

--- TRNG_GUI/GUI.py
+++ TRNG_GUI/GUI.py
@@ -1,4 +1,3 @@
-# import requests
 import tkinter as tk
 import sys
 
@@ -14,36 +13,44 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack() # packs all widgets into the main frame 
-        self.create_widgets() 
+        self.CreateWidgets() 
 
-    def ChangeText(self, textnew): #updates status depending on which button was pushed
+    def ChangeText(self, textnew): 
+    # updates status depending on which button was pushed
         self.statusB.config(text = textnew) 
+
+    def CloseWindow(self): 
+    # closes window - ends script
+        root.destroy()
 
     def CreateWidgets(self):
     # creating and packing buttons
-        self.startCameraB = tk.Button(self, text="Start Camera", command=self.startCamera)
+        self.startCameraB = tk.Button(self, text="Start Camera", command=self.StartCamera)
         self.startCameraB.pack(side="bottom")
 
-        self.stopCameraB = tk.Button(self, text="Stop Camera", command=self.stopCamera)
+        self.stopCameraB = tk.Button(self, text="Stop Camera", command=self.StopCamera)
         self.stopCameraB.pack(side="bottom")
 
-        self.startLightB = tk.Button(self, text="Start Lightbarrier", command=self.startLight)
+        self.startLightB = tk.Button(self, text="Start Lightbarrier", command=self.StartLight)
         self.startLightB.pack(side="bottom")
 
-        self.stopLightB = tk.Button(self, text="Stop Lightbarrier", command=self.stopLight)
+        self.stopLightB = tk.Button(self, text="Stop Lightbarrier", command=self.StopLight)
         self.stopLightB.pack(side="bottom")
 
-        self.startEngineB = tk.Button(self, text="Start Engine", command=self.startEngine)
+        self.startEngineB = tk.Button(self, text="Start Engine", command=self.StartEngine)
         self.startEngineB.pack(side="bottom")
 
-        self.stopEngineB = tk.Button(self, text="Stop Engine", command=self.stopEngine)
+        self.stopEngineB = tk.Button(self, text="Stop Engine", command=self.StopEngine)
         self.stopEngineB.pack(side="bottom")
 
-        self.startAllB = tk.Button(self, text="Start All", command=self.startAll)
+        self.startAllB = tk.Button(self, text="Start All", command=self.StartAll)
         self.startAllB.pack(side="bottom")
 
-        self.stopAllB = tk.Button(self, text="Stop All", command=self.stopAll)
+        self.stopAllB = tk.Button(self, text="Stop All", command=self.StopAll)
         self.stopAllB.pack(side="bottom")
+
+        self.closeB = tk.Button(self, text="Close Window", command=self.CloseWindow)
+        self.closeB.pack(side="bottom")
 
         self.result_label = tk.Label(self, text="Bedienung leicht gemacht")
         self.result_label.pack(side="top")
@@ -53,41 +60,40 @@ class Application(tk.Frame):
 
     
     def StartCamera(self):
-       # Objecttracking starts
-       self.changeText("Status: Camera started")
+    # Objecttracking starts
+       self.ChangeText("Status: Camera started")
        
 
     def StopCamera(self):
-        # Objecttracking stops
-        self.changeText("Status: Camera stopped")
+    # Objecttracking stops
+        self.ChangeText("Status: Camera stopped")
         
 
     def StartLight(self):
-        # Lightbarrier starts
-       #  Lightbarrier.__init__
-       self.changeText("Status: Lightbarrier started")
+    # Lightbarrier starts
+       self.ChangeText("Status: Lightbarrier started")
        
 
     def StopLight(self):
-        # Lightbarrier stops
-        self.changeText("Status: Lightbarrier stopped")
+    # Lightbarrier stops
+        self.ChangeText("Status: Lightbarrier stopped")
         
 
     def StartEngine(self):
-        # Engine Tool starts
-        self.changeText("Status: Engine started")
+    # Engine Tool starts
+        self.ChangeText("Status: Engine started")
 
     def StopEngine(self):
-        # Engine Tool stops
-        self.changeText("Status: Engine stopped")
+    # Engine Tool stops
+        self.ChangeText("Status: Engine stopped")
 
     def StartAll(self):
-        # All tools start
-        self.changeText("Status: All tools started")
+    # All tools start
+        self.ChangeText("Status: All tools started")
 
     def StopAll(self):
-        # All tools stop
-        self.changeText("Status: All tools stopped")
+    # All tools stop
+        self.ChangeText("Status: All tools stopped")
 
 
 root = tk.Tk() # setting root
