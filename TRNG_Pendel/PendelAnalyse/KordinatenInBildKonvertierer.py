2b0c81ae899556a80c5909029320c550f6b158d2
Paul Betsch <2120546@stud.hs-mannheim.de>
2023-05-10 15:58:46 +0200

    Pendelanalyse Skript kommentiert und aufgeräumt

--- TRNG_Pendel/PendelAnalyse/KordinatenInBildKonvertierer.py
+++ TRNG_Pendel/PendelAnalyse/KordinatenInBildKonvertierer.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 
 def generatePictureOutOfDictionary(dic):
-    img = Image.new('RGB', (1920, 1080), (0,0,0))
+    img = Image.new('RGB', (1200, 1200), (0,0,0))
 
     v = list(dic.values())
     maxValue = max(v)
@@ -11,15 +11,13 @@ def generatePictureOutOfDictionary(dic):
     
     valuesList = dic.values()
 
-    print(len(valuesList))
     minValue = min(valuesList)
     maxValue = max(valuesList)
     for tupel in dic:
-        #frequency = dic[tupel] / maxValue 
         frequency = ((dic[tupel] - minValue) / (maxValue - minValue)) * 0.8 + 0.2
         img.putpixel((tupel[0],tupel[1]), (int(frequency*255),int(frequency*255),int(frequency*255)))
         
             
         
     img.save('image.png')
-    print("saved")
+    print("Bild gespeichert")
