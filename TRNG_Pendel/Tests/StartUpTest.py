9b2f92798333f870dd975e3b7185932175353205
unknown <carlo@bauer-kothen.de>
2023-05-11 11:38:34 +0200

    implemented monobit and chi-squared goodness of fit test for startuptest

--- TRNG_Pendel/Tests/StartUpTest.py
+++ TRNG_Pendel/Tests/StartUpTest.py
@@ -1,5 +1,6 @@
 from math import fabs as fabs
 from math import sqrt as sqrt
+from scipy import stats
 from scipy.special import erfc as erfc
 
 def __contains_only_true(self, lst):
@@ -7,124 +8,63 @@ def __contains_only_true(self, lst):
 
 def StartUpTest(binary_String):
     binary_array = [int(x) for x in binary_String]
-    
-    if(__test_monobit(binary_array) and __poker_test(binary_array) and __boolean_test3(binary_array)):
+    #TODO: Test the tests and update to ensure 128 Bit array
+    if(__test_monobit(binary_array) and __chi2_gof_test(binary_array)):
         return True
     else:
         return False
 
 def __test_monobit(self, binary_String):
     """
-    Implements the Monobit Test on 20,000 bits sequences.
+    Implements the Monobit Test.
     Input:
         bits: an array with the bits as integers.
     Description and Evaluation rule:
         The Monobit Test is passed when the sum of all bits is in the interval [9654;10346]. Otherwise it failed.
     """
-    lastPosition = 0
-    results =[]
-    trueCounter = 0
+    length_of_bit_string = len(binary_String)
 
-    while(lastPosition+20000 <= len(binary_String)):
-        T1 = sum(binary_String[lastPosition:20000+lastPosition])
+    # Variable for S(n)
+    count = 0
+    # Iterate each bit in the string and compute for S(n)
+    for bit in binary_String:
+        if bit == 0:
+            # If bit is 0, then -1 from the S(n)
+            count -= 1
+        elif bit == 1:
+            # If bit is 1, then +1 to the S(n)
+            count += 1
 
-        if 9654 < T1 < 10346:
-            results.append(True)
-        else:
-            results.append(False)
-        lastPosition += 20000
-    
-    if(__contains_only_true(results)):
-        return True
-    else: 
-        return False
+    # Compute the test statistic
+    sObs = count / sqrt(length_of_bit_string)
 
-def __poker_test(binary_array):
+    # Compute p-Value
+    p_value = erfc(fabs(sObs) / sqrt(2))
+
+    # return a p_value
+    return p_value >= 0.01
+
+
+
+def __chi2_gof_test(self, binary_array):
     """
-    Implements the Poker Test on 20,000 bits sequences.
+    Implements a chi-squared goodness of fit test on a binary array.
     Input:
-        binary_array: an array with the bits as integers.
+        bits: an array with the bits as integers.
     Description and Evaluation rule:
-        The Poker Test is passed when T2 is in the interval [1.03;57.4]. Otherwise it failed.
-        It checks the occurences of 4-bit patterns.
+        The Test is passed when the P-Value is smaller than 1.0. Otherwise it failed.
     """
-    lastPosition = 0
-    results =[]
-    trueCounter = 0
+    # Define the expected frequencies assuming a discrete uniform distribution
+    n = len(binary_array)
+    expected_freq = [n/2]*2
 
-    while(lastPosition+20000 <= len(binary_array)):
-        bit_sequence = binary_array[lastPosition:lastPosition+20000]
-        f = [0] * 16
+    # Count the observed frequencies
+    observed_freq = [binary_array.count(0), binary_array.count(1)]
 
-        for j in range(5000):
-            c_j = 8*bit_sequence[4*j] + 4*bit_sequence[4*j+1] + \
-                2*bit_sequence[4*j+2] + bit_sequence[4*j+3]
-            f[c_j] += 1
+    # Perform the chi-squared goodness of fit test
+    testStat, pValue = stats.chisquare(observed_freq, expected_freq)
 
-        # Calculate T_2 using the formula given in the requirements
-        T_2 = (16/5000) * sum([freq**2 for freq in f]) - 5000
-
-        # Check if the sequence passes the test
-        if 1.03 < T_2 < 57.4:
-            results.append(True)
-        else:
-            results.append(False)
-        lastPosition += 20000
-    
-    if(__contains_only_true(results)):
-        return True
-    else: 
-        return False
-
-def __boolean_test3(binary_array):
-    """
-    Implements the Runs Test on 20,000 bits sequences.
-    Input:
-        binary_array: an array with the bits as integers.
-    Description and Evaluation rule:
-        The Runs Test is passed when the runs for specific lengths are in a defined interval. Otherwise it failed.
-        A run is defined as a consecutive sequence of the same number.
-    """
-    lastPosition = 1
-    results =[]
-    trueCounter = 0
-    bitfieldB = binary_array
-    lowerBound = [0, 2267, 1079, 502, 223, 90, 90]
-    upperBound = [0, 2733, 1421, 748, 402, 223, 223]
-    run = 1
-
-    while(lastPosition+20000 <= len(binary_array)):
-        run0field = [0] * 7
-        run1field = [0] * 7
-        for i in range(lastPosition, lastPosition+20000):
-            if bitfieldB[i-1] == bitfieldB[i]:
-                run += 1
-            else:
-                if run > 6:
-                    run = 6
-                if bitfieldB[i-1] == 1:
-                    run1field[run] += 1
-                else:
-                    run0field[run] += 1
-                run = 1
-        if run > 6:
-            run = 6
-        if bitfieldB[i-1] == 1:
-            run1field[run] += 1
-        else:
-            run0field[run] += 1
-        for i in range(1, 7):
-            if lowerBound[i] <= run0field[i] <= upperBound[i]:
-                results.append(True)
-            else:
-                results.append(False)
-
-            if lowerBound[i] <= run1field[i] <= upperBound[i]:
-                results.append(True)
-            else:
-                results.append(False)
-        lastPosition += 20000
-    if(__contains_only_true(results)):
-        return True
-    else: 
-        return False
+    #print(f"Test statistic: {testStat:.2f}")
+    #print(f"P-value: {pValue:.4f}")
+    # According to the BSI Standard PTG2 the test variable of the X2-Test must be less than 65.0
+    return testStat < 65.0
