b714ed6e74b995977fb5bf598656dd3bc372a17c
unknown <carlo@bauer-kothen.de>
2023-05-12 09:38:45 +0200

    fixed errors when running startuptest

--- TRNG_Pendel/Tests/StartUpTest.py
+++ TRNG_Pendel/Tests/StartUpTest.py
@@ -3,31 +3,34 @@ from math import sqrt as sqrt
 from scipy import stats
 from scipy.special import erfc as erfc
 
-def __contains_only_true(self, lst):
-    return all(element is True for element in lst)
-
-def StartUpTest(binary_String):
-    binary_array = [int(x) for x in binary_String]
-    #TODO: Test the tests and update to ensure 128 Bit array
-    if(__test_monobit(binary_array) and __chi2_gof_test(binary_array)):
+def StartUpTest(binaryArray):
+    """
+    Implements two statistical Test to ensure the provided Bit Array has a good randomness.
+    Input:
+        binaryArray: an array with bits. Canbe Provided by bin()[2:] method.
+    Return:
+        True if both statistical tests (monobit and chi-squared gof) are successful. False if one of them is not passed.
+    """
+    #TODO: Test the tests and update to ensure X Bit array is used
+    if(__test_monobit(binaryArray) and __chi2_gof_test(binaryArray)):
         return True
     else:
         return False
 
-def __test_monobit(self, binary_String):
+def __test_monobit(binaryArray):
     """
-    Implements the Monobit Test.
+    Implements the Monobit Test on a binary array.
     Input:
         bits: an array with the bits as integers.
     Description and Evaluation rule:
         The Monobit Test is passed when the sum of all bits is in the interval [9654;10346]. Otherwise it failed.
     """
-    length_of_bit_string = len(binary_String)
+    arrayLength = len(binaryArray)
 
     # Variable for S(n)
     count = 0
     # Iterate each bit in the string and compute for S(n)
-    for bit in binary_String:
+    for bit in binaryArray:
         if bit == 0:
             # If bit is 0, then -1 from the S(n)
             count -= 1
@@ -36,17 +39,17 @@ def __test_monobit(self, binary_String):
             count += 1
 
     # Compute the test statistic
-    sObs = count / sqrt(length_of_bit_string)
+    sObs = count / sqrt(arrayLength)
 
     # Compute p-Value
-    p_value = erfc(fabs(sObs) / sqrt(2))
+    pValue = erfc(fabs(sObs) / sqrt(2))
 
     # return a p_value
-    return p_value >= 0.01
+    return pValue >= 0.01
 
 
 
-def __chi2_gof_test(self, binary_array):
+def __chi2_gof_test(binaryArray):
     """
     Implements a chi-squared goodness of fit test on a binary array.
     Input:
@@ -54,17 +57,23 @@ def __chi2_gof_test(self, binary_array):
     Description and Evaluation rule:
         The Test is passed when the P-Value is smaller than 1.0. Otherwise it failed.
     """
+    print("Bits: " + binaryArray)
     # Define the expected frequencies assuming a discrete uniform distribution
-    n = len(binary_array)
-    expected_freq = [n/2]*2
+    n = len(binaryArray)
+
+    # Convert to binary string for observed_frequency
+    binaryString = str(binaryArray)
 
     # Count the observed frequencies
-    observed_freq = [binary_array.count(0), binary_array.count(1)]
+    observed_freq = [binaryString.count('0'), binaryString.count('1')]
+
+    # Calculate the expected frequencies
+    expected_freq = [n/2, n/2]
 
     # Perform the chi-squared goodness of fit test
     testStat, pValue = stats.chisquare(observed_freq, expected_freq)
 
-    #print(f"Test statistic: {testStat:.2f}")
-    #print(f"P-value: {pValue:.4f}")
-    # According to the BSI Standard PTG2 the test variable of the X2-Test must be less than 65.0
-    return testStat < 65.0
+    print(f"Test statistic: {testStat:.2f}")
+    print(f"P-value: {pValue:.4f}")
+    # According to the BSI Standard PTG2 the test variable of the X2-Test must be less than 65.0 # TODO: Figure out which variable should be tested
+    return testStat < 0.65
