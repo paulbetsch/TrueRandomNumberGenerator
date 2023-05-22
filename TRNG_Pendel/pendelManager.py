d2ee19ec4f6861b748400c4b13abe08f393c3876
unknown <carlo@bauer-kothen.de>
2023-05-22 13:38:12 +0200

    Updated PendelManager

--- TRNG_Pendel/pendelManager.py
+++ TRNG_Pendel/pendelManager.py
@@ -16,8 +16,52 @@ __manager = ''
 # Die klasse sollte intern das multiprocessing verwalten
 class PendelManager:
     def __init__(self, controlledByAPI):
-        self.manager = Manager()
+        #self.manager = Manager()
         pass
+    
+    def __cut_string(self, string, position, length):
+        # Determine the start and end indices for slicing
+        start_index = position
+        end_index = position + length
+        
+        # Perform the string slicing
+        cut_out_string = string[:start_index] + string[end_index:]
+        
+        return cut_out_string
+
+    def __prepareBinaryStringForReturn(self, binaryString, quantity, numBits):
+        result = []
+        numHexDigits = (numBits + 3) // 4
+        
+        position = 0
+        # Get as many Random Numbers as required
+        for i in range(0, quantity):
+            # Get Random Bits from NoiseSource
+            randomBits = self.__cut_string(binaryString, position, numBits)
+            # Convert them into hex number
+            randomHex = hex(int(randomBits, 2))
+            # If necesarry remove leading "0x"
+            hexStr = str(randomHex)[2:]
+            # Prepend the necessary number of leading zeroes to the hex string
+            hexStr = hexStr.zfill(numHexDigits)
+            # Append the Hex Number to the array
+            result.append(hexStr)
+            # change position for next iteration:
+            position += numBits
+        return result
+
+    def __hexArrayToBinaryArray(self, hex_array):
+        binary_array = []
+
+        for hex_string in hex_array:
+            for hex_digit in hex_string:
+                # Convert the hex digit to a 4-bit binary string
+                binary_digit = bin(int(hex_digit))[2:]
+                
+                # Convert the binary digit string to a binary array
+                binary_array.extend([int(bit) for bit in binary_digit])
+        
+        return binary_array
 
     # Wird später aufgerufen um die Funktionalität der Lichtschranke, der Kamera und der Motorisierung des Pendels zu gewährleisten.
     def checkFunctionality(self):
@@ -26,9 +70,14 @@ class PendelManager:
         engineWorks = engineFunc.CheckEngineFunctionality()
         magnetWorks = magnetFunc.CheckMagnetFunctionality()
 
-        # Only functional if all components function correctly
-        return camWorks and engineWorks and magnetWorks
+        # Check if noise source works correctly
+        hexNums = self.generateRandomBits(18, 100)
+        # convert hexNums to binary
+        binaryData = self.__hexArrayToBinaryArray(hexNums)
+        noiseSourceWorks = self.checkBSITests(binaryData)
 
+        # Only functional if all components function correctly
+        return camWorks and engineWorks and magnetWorks and noiseSourceWorks
 
     # Hier soll der Motor gesteuert werden, und die Werte der Kamera und der Lichtschranke ausgewertet werden
     # Aktuell zu Testzwecken werden hier nur pseudozufallszahlen generiert
@@ -47,18 +96,18 @@ class PendelManager:
 
             # Do checks with numbers and generate as much as the params require here
             byts = []
-            goodByts = []
+            goodBytes = ""
             failCounter = 0
-
+            byte = byte
             while not errorEvent.is_set():
 
                 if(randomValues.qsize >= 8):
                     for i in range(0, 8):
                         byts.append(randomValues.get())
 
-                    if(len(byts) >= 128):
+                    if(len(byts) >= 8):
                         if(online.onlineTest(byts)):
-                            goodByts += byts
+                            goodBytes += byts
                             failCounter = 0
                         elif(failCounter + 1 == 10):
                             errorEvent.set()
@@ -66,21 +115,24 @@ class PendelManager:
                             failCounter += 1
                         byts = []
 
-                    if(goodByts == (quantity * numBits)):
+                    if(len(goodBytes) == (quantity * numBits)):
+                        # Stop the generation of random values
+                        stopEvent.set()
                         break
                 else:
-                    # TODO: implement possible timeout, when no bytes are retrieved from videoProc
+                    # polling rate at 0.1 seconds for main process
                     time.sleep(0.1)
             
-            # TODO: prepare goodByts for return
+            #prepare goodByts for return (split into quantitty times numBits and change to hex numbers)
+            result = self.__prepareBinaryStringForReturn(goodBytes, quantity, numBits)
+
+
             # TODO: errorEvent handling
             if(errorEvent.is_set()):
-                pass
+                raise Exception("An Error Occured: Pendullum not moving.")
             else:
                 pass
 
-            # Stop the generation of random values
-            stopEvent.set()
             # End Process
             videoProc.terminate()
 
@@ -88,7 +140,7 @@ class PendelManager:
 
     # Statische Prüfung der Zufallszahlen
     def checkBSITests(self, binaryData):
-        tf.TotalFailureTest(str(binaryData), False, 8)
+        return toft.TotalFailureTest(str(binaryData), False, 8) and startUp.StartUpTest(str(binaryData))
 
 # Returns the only instance of the PendelManger class
 def GetInstance():
@@ -99,10 +151,11 @@ if __name__ == '__main__':
     print("Executed when called directly")
     # Eventuell können wir hier eine Menüführung über CLI implementieren
     __CONTROLLED_BY_API = False
+    print("Before Cstor")
     __manager = PendelManager(__CONTROLLED_BY_API)
-    
+    print("After cstor")
     # For Testing:
-    __manager.generateRandomBits(10, 100)
+    #__manager.generateRandomBits(10, 100)
 
 # oder ob er von der API aus aufgerufen wird.            
 else:
