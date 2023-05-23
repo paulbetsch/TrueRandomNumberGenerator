293f3c785428e88233d71b449aa7d0b6cfeb7cd0
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-05-23 14:02:16 +0200

    Update FunctionalityTestMagnet.py
    Implemented Test for HubMagnet

--- TRNG_Pendel/Tests/FunctionalityTestMagnet.py
+++ TRNG_Pendel/Tests/FunctionalityTestMagnet.py
@@ -1,3 +1,39 @@
-# This method checks the functionality of the lifting Magnet
-def CheckMagnetFunctionality():
+from ina219 import INA219
+from ina219 import DeviceRangeError
+import time
+import RPi.GPIO as GPIO
+
+SHUNT_OHMS = 0.1
+MAX_EXPECTED_AMPS = 0.2
+"""
+Checks the Functionalty of the Magnet
+"""
+def read():
+    ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS)
+    ina.configure(ina.RANGE_16V)
+
+
+    current = ina.current()*-1
+    voltage = ina.shunt_voltage()*-1
+    #print("Current:" + str(current))
+    #print("Voltage:" + str(voltage))
+
+    if voltage<5:
+        GPIO.output(13,1)      
+        return False
+    
+    GPIO.output(13,1)   
     return True
+        
+      
+def checkIt():
+    GPIO.setwarnings(False)
+    
+    GPIO.setmode(GPIO.BCM)
+    
+    #Magnet
+    GPIO.setup(13, GPIO.OUT)  
+         
+    while True:
+        GPIO.output(13,0)   
+        return read()
